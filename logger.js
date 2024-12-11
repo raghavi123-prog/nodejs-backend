954a2d7620d482b28848906cb694bab2f7f4a383
Vani Satya Sai Raghaveswari Mekala <mekalaraghavi@gmail.com>
2024-12-11 21:39:49 +0530

    logger.js

---
+++ logger.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+require('winston-cloudwatch');
+
+const logger = winston.createLogger({
+    level: 'info',
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.CloudWatch({
+            logGroupName: 'NodeJS-Backend-Logs',
+            logStreamName: `${process.env.NODE_ENV || 'development'}-Backend-App-Stream`,
+            awsRegion: process.env.AWS_REGION || 'your-region',
+            jsonMessage: true,
+        }),
+    ],
+});
+
+module.exports = logger;
